cce36eb85e7bbb8f159796beab15d9f9d99a0674
Fabienne RONDI <f.rondi@orange.fr>
2021-09-30 21:03:24 +0200

    Test login succesful

--- server.py
+++ server.py
@@ -3,6 +3,20 @@ from flask import Flask, render_template, request, redirect, flash, url_for, \
     abort, session
 
 
+def load_clubs():
+        with open('clubs.json') as c:
+            listOfClubs = json.load(c)['clubs']
+            return listOfClubs
+
+def load_competitions():
+        with open('competitions.json') as comps:
+            listOfCompetitions = json.load(comps)['competitions']
+            return listOfCompetitions
+
+def research_club_in_clubs(clubs, email):
+    return [club for club in clubs if club['email'] == email][0]
+
+
 def create_app(config):
 
     app = Flask(__name__)
@@ -11,19 +25,9 @@ def create_app(config):
 
     # ########## Loading  datas from JSON files ################
 
-
-    def loadClubs():
-        with open('clubs.json') as c:
-            listOfClubs = json.load(c)['clubs']
-            return listOfClubs
-
-    def loadCompetitions():
-        with open('competitions.json') as comps:
-            listOfCompetitions = json.load(comps)['competitions']
-            return listOfCompetitions
         
-    competitions = loadCompetitions()
-    clubs = loadClubs()
+    competitions = load_competitions()
+    clubs = load_clubs()
 
 
     # ################# ROUTING ###################
@@ -35,33 +39,32 @@ def create_app(config):
         return render_template('index.html')
 
 
-    @app.route('/login', methods=('GET', 'POST'))
+    @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
             email = request.form['email']
             try:
-                logged_club = [club for club in clubs if club['email'] == email][0]
+                logged_club = research_club_in_clubs(clubs, email)
             except IndexError:
                 error = 'Unknown club. Sorry.'
             else :
                 error = None
-            
-            if error is None:
+                session['name'] = logged_club['name']
                 session.clear()
                 session['email'] = email
                 return redirect(url_for('showSummary'))
-            
+                        
             flash(error)
         
         return render_template('login.html')
 
 
-    @app.route('/showSummary', methods=('GET'))
+    @app.route('/showSummary', methods=['GET'])
     def showSummary():
         club = [
             club for club in clubs if club['email'] == session['email']
             ][0]
-        session['name'] = club['name']
+        
         return render_template('welcome.html',
                             club=club,
                             competitions=competitions)
--- tests/test_unitaire/test_auth.py
+++ tests/test_unitaire/test_auth.py
@@ -1,42 +1,73 @@
 import pytest
 
 from flask import session
-import server
+from server import research_club_in_clubs
 from tests.conftest import client
 
 
 @pytest.fixture
-def club_test():
-    club = {
-        'name': 'club_test_name',
-        'email': 'club_test@mail.com',
-        'points': '15'
-    }
-    return club
+def club_one():
+    club_one = {'name': 'club_test1_name',
+              'email': 'club_test1@mail.com',
+              'points': '15'}
+    return club_one
+
+@pytest.fixture
+def clubs():
+    clubs = [{'name': 'club_test1_name',
+              'email': 'club_test1@mail.com',
+              'points': '15'},
+             {'name': 'club_test2_name',
+              'email': 'club_test2@mail.com',
+              'points': '8'}     ]        
+    return clubs
+
+def login(client, email):
+    return(client.post('/login', data={'email': email}, follow_redirects=True))
 
 
 def test_root_status_ok(client):
     response = client.get('/')
     assert response.status_code == 200
 
+
 def test_index_status_ok(client):
     response = client.get('/index')
     assert response.status_code == 200
+
 
 def test_login_page_access_ok(client):
     response = client.get('/login')
     assert response.status_code == 200
 
 
+def test_login_succesful(client, club_one, mocker):
+    mocker.patch('server.research_club_in_clubs', return_value=club_one)
+
+    login(client, 'club_test1@mail.com')
+    assert session['email'] == 'club_test1@mail.com'
 
 
+def test_research_clubs_in_clubs(clubs, club_one):
+    result = research_club_in_clubs(clubs, 'club_test1@mail.com')
+    assert result == club_one
+
+
+def test_login_unsuccesful(client, club_one, mocker):
+    mocker.patch('server.research_club_in_clubs', return_value=None)
+
+    login(client, 'pas_bon@mail.com')
+    assert session == {}
 
 
 def test_logout_redirection(client):
     response = client.get('/logout')
     assert response.status_code == 302
 
-def test_logout_session_cleared(client):
-    response = client.get('/logout')
-    assert session == {}
 
+def test_logout_session_cleared(client, mocker, club_one):    
+    mocker.patch('server.research_club_in_clubs', return_value=club_one)
+    login(client, 'club_test1@mail.com')
+
+    client.get('/logout')
+    assert session == {}
