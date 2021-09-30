4a1518078ccbe616f417cd54b0022640eb758fc7
Fabienne RONDI <f.rondi@orange.fr>
2021-09-30 22:37:53 +0200

    flake8

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for, \
-    abort, session
+    session
 
 
 def load_clubs():
-        with open('clubs.json') as c:
-            listOfClubs = json.load(c)['clubs']
-            return listOfClubs
+    with open('clubs.json') as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
+
 
 def load_competitions():
-        with open('competitions.json') as comps:
-            listOfCompetitions = json.load(comps)['competitions']
-            return listOfCompetitions
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
+
 
 def research_club_in_clubs(clubs, email):
     return [club for club in clubs if club['email'] == email][0]
@@ -25,19 +27,16 @@ def create_app(config):
 
     # ########## Loading  datas from JSON files ################
 
-        
     competitions = load_competitions()
     clubs = load_clubs()
 
-
     # ################# ROUTING ###################
-
 
     @app.route('/')
     @app.route('/index')
     def index():
-        return render_template('index.html')
 
+        return render_template('index.html')
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
@@ -47,26 +46,25 @@ def create_app(config):
                 logged_club = research_club_in_clubs(clubs, email)
             except IndexError:
                 error = 'Unknown club. Sorry.'
-            else :
+            else:
                 error = None
-                session['name'] = logged_club['name']
                 session.clear()
+                session['name'] = logged_club['name']
                 session['email'] = email
+                print(session)
                 return redirect(url_for('showSummary'))
-                        
-            flash(error)
-        
-        return render_template('login.html')
 
+            flash(error)
+
+        return render_template('login.html')
 
     @app.route('/showSummary', methods=['GET'])
     def showSummary():
         club = research_club_in_clubs(clubs, session['email'])
-        
-        return render_template('welcome.html',
-                            club=club,
-                            competitions=competitions)
 
+        return render_template('welcome.html',
+                               club=club,
+                               competitions=competitions)
 
     @app.route('/book/<competition>/<club>')
     def book(competition, club):
@@ -74,14 +72,13 @@ def create_app(config):
         foundCompetition = [c for c in competitions if c['name'] == competition][0]
         if foundClub and foundCompetition:
             return render_template('booking.html',
-                                club=foundClub,
-                                competition=foundCompetition)
+                                   club=foundClub,
+                                   competition=foundCompetition)
         else:
             flash("Something went wrong-please try again")
             return render_template('welcome.html',
-                                club=club,
-                                competitions=competitions)
-
+                                   club=club,
+                                   competitions=competitions)
 
     @app.route('/purchasePlaces', methods=['POST'])
     def purchasePlaces():
@@ -94,16 +91,14 @@ def create_app(config):
         -placesRequired
         flash('Great-booking complete!')
         return render_template('welcome.html',
-                            club=club,
-                            competitions=competitions)
-
+                               club=club,
+                               competitions=competitions)
 
     # TODO: Add route for points display
-
 
     @app.route('/logout')
     def logout():
         session.clear()
         return redirect(url_for('index'))
-    
+
     return app
--- tests/test_unitaire/test_auth.py
+++ tests/test_unitaire/test_auth.py
@@ -2,15 +2,15 @@ import pytest
 
 from flask import session
 from server import research_club_in_clubs
-from tests.conftest import client
 
 
 @pytest.fixture
 def club_one():
     club_one = {'name': 'club_test1_name',
-              'email': 'club_test1@mail.com',
-              'points': '15'}
+                'email': 'club_test1@mail.com',
+                'points': '15'}
     return club_one
+
 
 @pytest.fixture
 def clubs():
@@ -19,8 +19,9 @@ def clubs():
               'points': '15'},
              {'name': 'club_test2_name',
               'email': 'club_test2@mail.com',
-              'points': '8'}     ]        
+              'points': '8'}]
     return clubs
+
 
 def login(client, email):
     return(client.post('/login', data={'email': email}, follow_redirects=True))
@@ -48,31 +49,31 @@ def test_research_clubs_in_clubs(clubs, club_one):
 
 def test_login_succesful(client, club_one, mocker):
     client.get('/logout')
-    
+
     mocker.patch('server.research_club_in_clubs', return_value=club_one)
     login(client, 'club_test1@mail.com')
-    
+
     assert session['email'] == 'club_test1@mail.com'
 
 
-def test_login_unsuccesful(client, club_one, mocker):
+def test_login_unsuccesful(client):
     client.get('/logout')
-    
+
     response = login(client, 'pas_bon@mail.com')
-    
+
     assert response.status_code == 200
     assert b'Unknown club. Sorry.' in response.data
     assert session == {}
-    
+
 
 def test_logout_redirection(client):
     response = client.get('/logout')
     assert response.status_code == 302
 
 
-def test_logout_session_cleared(client, mocker, club_one):    
+def test_logout_session_cleared(client, mocker, club_one):
     mocker.patch('server.research_club_in_clubs', return_value=club_one)
     login(client, 'club_test1@mail.com')
 
-    response = client.get('/logout')
+    client.get('/logout')
     assert session == {}
