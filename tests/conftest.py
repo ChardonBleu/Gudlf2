205acffd7447d07bed210dd51d1c4b2f1d05c4b7
Fabienne RONDI <f.rondi@orange.fr>
2021-10-05 20:16:59 +0200

    update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,43 @@
 import pytest
+from flask import template_rendered
 from server import create_app
+from tests.fixtures import competitions, club_one
 
 
 @pytest.fixture
-def client():
+def app(mocker, competitions):
+    mocker.patch('server.load_competitions',
+                     return_value=competitions)
     app = create_app({"TESTING": True})
+    return app
+
+
+@pytest.fixture
+def client(app):
     with app.test_client() as client:
         yield client
 
 
 @pytest.fixture
-def logged_client(mocker):
-    app = create_app({"TESTING": True})
-    with app.test_client() as client:
+def logged_client(app, mocker, club_one):
+    with app.test_client() as client:        
         mocker.patch('server.research_club_in_clubs_by_email',
-                     return_value={'name': 'club_test1_name',
-                                   'email': 'club_test1@mail.com',
-                                   'points': '15'})
+                     return_value=club_one)
+        
         client.post('/login',
                     data={'email': 'club_test1_name'},
                     follow_redirects=True)
         yield client
+
+@pytest.fixture
+def captured_templates(app):
+    recorded = []
+
+    def record(sender, template, context, **extra):
+        recorded.append((template, context))
+
+    template_rendered.connect(record, app)
+    try:
+        yield recorded
+    finally:
+        template_rendered.disconnect(record, app)
