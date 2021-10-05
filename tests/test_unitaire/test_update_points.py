91c4e1b55c53c025bbf987a29fadeeef7679e130
Fabienne RONDI <f.rondi@orange.fr>
2021-10-05 13:30:17 +0200

    Test file

---
+++ tests/test_unitaire/test_update_points.py
@@ -0,0 +1,6 @@
+import pytest
+
+from server import research_club_in_clubs_by_name
+from server import research_competition_in_competitions_by_name
+from tests.fixtures import club_one, clubs, competition_one, competitions
+
