76d39f86303216836897dd45c5f3128fed831e50
Fabienne RONDI <f.rondi@orange.fr>
2021-10-07 16:44:38 +0200

    flake8

--- tests/test_integration/test_intégration_max_12_places.py
+++ tests/test_integration/test_intégration_max_12_places.py
@@ -1,9 +1,22 @@
 from tests.fixtures import club_one, competition_one
 
 
-def test_purchase_places_with_update_points(client, mocker,
-                                            competition_one, club_one,
-                                            captured_templates):
+def test_purchase_places_with_update_points(client, mocker, competition_one,
+                                            club_one, captured_templates):
+    """when logged user is redirected to welcome page.
+    Then he can choose a competition de book places on it.
+    If he tries to book more than 12 places he receive an alert message.
+    He stays on booking page and he can try again.
+    If he purchase for less than 12 places he is redirected to competitions
+    page with a confirmation message and competition places are updated. 
+
+    Arguments:
+        client {[type]} -- [description]
+        mocker {[type]} -- [description]
+        competition_one {[type]} -- [description]
+        club_one {[type]} -- [description]
+        captured_templates {[type]} -- [description]
+    """
     response = client.get('/login')
     assert response.status_code == 200
     template1, context1 = captured_templates[0]
@@ -12,7 +25,7 @@ def test_purchase_places_with_update_points(client, mocker,
     mocker.patch('server.research_club_in_clubs_by_email',
                  return_value=club_one)
     response = client.post('/login', data={'email': 'club_test1@mail.com'},
-                follow_redirects=True)
+                           follow_redirects=True)
     template2, context2 = captured_templates[1]
     assert template2.name == "welcome.html"
 
@@ -27,12 +40,21 @@ def test_purchase_places_with_update_points(client, mocker,
     assert template3.name == "booking.html"
     initial_competition_places = int(context3['competition']['numberOfPlaces'])
 
+    booked_places = 13
+    response = client.post('/purchasePlaces',
+                           data={'competition': "Compet du printemps",
+                                 'club': 'club_test1_name',
+                                 'places': str(booked_places)},
+                           follow_redirects=True)
+    template4, context4 = captured_templates[3]
+    assert template4.name == "booking.html"
+
     booked_places = 4
     response = client.post('/purchasePlaces',
-                                  data={'competition': "Compet du printemps",
-                                        'club': 'club_test1_name',
-                                        'places': str(booked_places)},
-                                  follow_redirects=True)
-    template4, context4 = captured_templates[3]
-    assert template4.name == "competitions.html"
+                           data={'competition': "Compet du printemps",
+                                 'club': 'club_test1_name',
+                                 'places': str(booked_places)},
+                           follow_redirects=True)
+    template5, context5 = captured_templates[4]
+    assert template5.name == "competitions.html"
     assert context4['competition']['numberOfPlaces'] == initial_competition_places - booked_places
