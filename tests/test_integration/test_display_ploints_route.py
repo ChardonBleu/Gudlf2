f07edc08ff7acfecf235eb9ecad673bf9041e3b6
Fabienne RONDI <f.rondi@orange.fr>
2021-10-07 10:27:36 +0200

    update conftest

--- tests/test_integration/test_display_ploints_route.py
+++ tests/test_integration/test_display_ploints_route.py
@@ -4,9 +4,15 @@ from tests.fixtures import club_one
 
 def test_login_display_points_successful(client, mocker, club_one):
     
-    
-    
+    response = client.get('/login')
+    assert response.status_code == 200
     
     mocker.patch('server.research_club_in_clubs_by_email',
                  return_value=club_one)
+    response = client.post('/login', data={'email': 'club_test1@mail.com'},
+                follow_redirects=True)
+    
+    assert b'Welcome, club_test1@mail.com. You\'re logged in' in response.data
+    
+    
     response = client.get('/welcome')
--- tests/test_unitaire/test_points.py
+++ tests/test_unitaire/test_points.py
@@ -9,7 +9,7 @@ def test_welcome_for_display_points(logged_client, club_one, mocker):
     response = logged_client.get('/welcome')
     assert response.status_code == 200
     assert b'All clubs points:' in response.data
-    assert b' <td>Iron Temple</td>\n                    <td>4</td>' in response.data
+    assert b'<td>club_test1_name</td>\n                    <td>15</td>' in response.data
 
 
 def test_redirect_if_non_logged(client):
