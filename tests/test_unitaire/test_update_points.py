e4d296b1e2e07f13bdad5f51d06810e47a8dff11
Fabienne RONDI <f.rondi@orange.fr>
2021-10-07 16:58:50 +0200

    flake8

--- tests/test_unitaire/test_update_points.py
+++ tests/test_unitaire/test_update_points.py
@@ -1,8 +1,3 @@
-import pytest
-from flask import Flask
-
-from server import research_club_in_clubs_by_name
-from server import research_competition_in_competitions_by_name
 from tests.fixtures import club_one, competition_one
 
 
