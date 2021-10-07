c093665d7c2bd2dc67241eae6db0a31b048bc36f
Fabienne RONDI <f.rondi@orange.fr>
2021-10-07 15:08:09 +0200

    flake8

--- tests/test_unitaire/test_public_tab.py
+++ tests/test_unitaire/test_public_tab.py
@@ -1,13 +1,8 @@
-from os import stat_result
-from re import template
-import pytest
-from flask import Flask
-
-
 def test_public_tab_with_non_logged_client(client):
     """
-    """    
-    
+    Tab with all club points is displayed on index page.
+    It's not necesary to be logged to see it.
+    """
     response = client.get('/index')
     assert response.status_code == 200
     assert b'Club name' in response.data
