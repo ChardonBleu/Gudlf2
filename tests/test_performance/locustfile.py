69d203f8bc0766f21399dca7f4f476b3e7895c48
Fabienne RONDI <f.rondi@orange.fr>
2021-10-11 22:04:25 +0200

    performance test on index, login, welcome, showSummary

--- tests/test_performance/locustfile.py
+++ tests/test_performance/locustfile.py
@@ -8,4 +8,29 @@ class ProjectPerfTest(HttpUser):
     @task
     def login(self):
         self.client.get('login')
-
+        self.client.post('login', {'email': 'club_test1_name'})
+    
+    @task
+    def welcome(self):
+        self.client.post('login', {'email': 'club_test1_name'})
+        self.client.get('welcome')
+    
+    @task
+    def competitions(self):
+        self.client.post('login', {'email': 'club_test1_name'})
+        self.client.get('showSummary')
+    
+    @task
+    def book(self):
+        self.client.post('login', {'email': 'club_test1_name'})
+        self.client.get('book/<competition_name>/<club_name>')
+    
+    @task
+    def purchase_with_update_points_club_and_nb_places(self):
+        self.client.post('login', {'email': 'club_test1_name'})
+        response = self.client.post('purchasePlaces',
+                         {'competition': 'Compet du printemps',
+                          'club': 'club_test1_name',
+                          'places': '2'})
+        print("Response status code:", response.status_code)
+        print("Response text:", response.text)
