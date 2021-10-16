44e7f12eff00a4e253ee596fd6d5035350c10558
Fabienne RONDI <f.rondi@orange.fr>
2021-10-16 18:01:15 +0200

    adding doctrings and id on template competitions

--- tests/test_fonctionnel/test_front_allowed_points.py
+++ tests/test_fonctionnel/test_front_allowed_points.py
@@ -12,32 +12,50 @@ from server import create_app
 
 
 CLUBS = [{'name': 'club_test1_name',
-            'email': 'club_test1@mail.com',
-            'points': '15'},
-            {'name': 'club_test2_name',
-            'email': 'club_test2@mail.com',
-            'points': '8'}]
+          'email': 'club_test1@mail.com',
+          'points': '15'},
+         {'name': 'club_test2_name',
+          'email': 'club_test2@mail.com',
+          'points': '8'}]
 
 CLUB_ONE = {'name': 'club_test1_name',
-                'email': 'club_test1@mail.com',
-                'points': '15'}
+            'email': 'club_test1@mail.com',
+            'points': '15'}
 
 COMPETITIONS = [{"name": "Compet du printemps",
-                    "date": "2040-04-01 10:00:00",
-                    "numberOfPlaces": "10"},
-                    {"name": "Compet des gros costauds",
-                    "date": "2035-08-15 13:30:00",
-                    "numberOfPlaces": "18"},
-                    {"name": "Compet des vieux balèzes",
-                    "date": "2018-08-15 13:30:00",
-                    "numberOfPlaces": "23"}]
+                 "date": "2040-04-01 10:00:00",
+                "numberOfPlaces": "10"},
+                {"name": "Compet des gros costauds",
+                 "date": "2035-08-15 13:30:00",
+                 "numberOfPlaces": "18"},
+                {"name": "Compet des vieux balèzes",
+                 "date": "2018-08-15 13:30:00",
+                 "numberOfPlaces": "23"}]
 
 
 class TestAllowedPoints(LiveServerTestCase):
+    """This file CAN'T BE USED ON WINDOWS. LiveServerTestCase isn't
+    supported on windows.
+
+    Arguments:
+        LiveServerTestCase {[type]} -- Is used to run flask app for selenium
+        testing
+
+    """
 
     @patch('server.load_competitions')
     @patch('server.load_clubs')
-    def create_app(self, mock_load_clubs, mock_load_competitions):        
+    def create_app(self, mock_load_clubs, mock_load_competitions):
+        """Create flask app for testing with mocked datas for clubs and
+        competitions
+
+        Arguments:
+            mock_load_clubs {[type]} -- mock clubs list
+            mock_load_competitions {[type]} -- mock competitions list
+
+        Returns:
+            flask app -- [description]
+        """
         mock_load_clubs.return_value = CLUBS
         mock_load_competitions.return_value = COMPETITIONS
         app = create_app({"TESTING": True})
@@ -47,15 +65,31 @@ class TestAllowedPoints(LiveServerTestCase):
         return app
 
     def setUp(self):
-        """Setup the test driver and create test users"""
+        """Setup the test driver on Chrome web browser"""
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.get_server_url())
 
     def tearDown(self):
+        """close web browser and server"""
         self.driver.quit()
 
     @patch('server.research_club_in_clubs_by_email')
     def test_booking_places_ok(self, mock_research_club_in_clubs_by_email):
+        """Test logging and places purchase.
+        Logged club is:
+        {'name': 'club_test1_name',
+         'email': 'club_test1@mail.com',
+         'points': '15'}
+        chossen competition is:
+        {"name": "Compet du printemps",
+         "date": "2040-04-01 10:00:00",
+         "numberOfPlaces": "10"}
+        In this test logged club want to purchase 2 places.
+        After booking clubs has 13 points left. 
+
+        Arguments:
+            mock_research_club_in_clubs_by_email {[type]} -- mock logged club
+        """
         mock_research_club_in_clubs_by_email.return_value = CLUB_ONE
         
         self.driver.get(self.get_server_url() + "/login")
@@ -83,6 +117,22 @@ class TestAllowedPoints(LiveServerTestCase):
 
     @patch('server.research_club_in_clubs_by_email')
     def test_not_enough_points(self, mock_research_club_in_clubs_by_email):
+        """Test logging and places purchase.
+        Logged club is:
+        {'name': 'club_test1_name',
+         'email': 'club_test1@mail.com',
+         'points': '15'}
+        chossen competition is:
+        {"name": "Compet des gros costauds",
+         "date": "2035-08-15 13:30:00",
+         "numberOfPlaces": "18"}
+        In this test logged club want to purchase 16 places.
+        It's impossible.  Appli show message:
+        'Not enough points available. Sorry.'
+
+        Arguments:
+            mock_research_club_in_clubs_by_email {[type]} -- mock logged club
+        """
         mock_research_club_in_clubs_by_email.return_value = CLUB_ONE
 
         self.driver.get(self.get_server_url() + "/login")
@@ -108,6 +158,22 @@ class TestAllowedPoints(LiveServerTestCase):
 
     @patch('server.research_club_in_clubs_by_email')
     def test_not_enough_places(self, mock_research_club_in_clubs_by_email):
+        """Test logging and places purchase.
+        Logged club is:
+        {'name': 'club_test1_name',
+         'email': 'club_test1@mail.com',
+         'points': '15'}
+        chossen competition is:
+        {"name": "Compet du printemps",
+         "date": "2040-04-01 10:00:00",
+         "numberOfPlaces": "10"}
+        In this test logged club want to purchase 12 places.
+        It's impossible.  Appli show message:
+        'Not enough places available. Sorry'
+
+        Arguments:
+            mock_research_club_in_clubs_by_email {[type]} -- mock logged club
+        """
         mock_research_club_in_clubs_by_email.return_value = CLUB_ONE
 
         self.driver.get(self.get_server_url() + "/login")
@@ -130,4 +196,3 @@ class TestAllowedPoints(LiveServerTestCase):
         self.driver.find_element_by_tag_name('button').click()
         time.sleep(2)
         assert 'Not enough places available. Sorry' in self.driver.page_source
-
