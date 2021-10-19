782c955f81a8c0d00083f7bdb17142ba634a6d4c
Fabienne RONDI <f.rondi@orange.fr>
2021-10-19 10:01:05 +0200

    integration test showSummary route

--- tests/test_integration/test_book_when_logged.py
+++ tests/test_integration/test_book_when_logged.py
@@ -31,15 +31,19 @@ def test_book_route(client, mocker, club_one, competition_one,
     mocker.patch('server.research_competition_in_competitions_by_name',
                  return_value=None)
 
-    response = client.get('/book/<competition_name>/<club_name>',
-                          follow_redirects=True)
+    response = response = client.get('/showSummary', follow_redirects=True)
     template3, context3 = captured_templates[2]
     assert template3.name == "competitions.html"
+
+    response = client.get('/book/<competition_name>/<club_name>',
+                          follow_redirects=True)
+    template4, context4 = captured_templates[3]
+    assert template4.name == "competitions.html"
 
     mocker.patch('server.research_competition_in_competitions_by_name',
                  return_value=competition_one)
 
     response = client.get('/book/<competition_name>/<club_name>',
                           follow_redirects=True)
-    template4, context4 = captured_templates[3]
-    assert template4.name == "booking.html"
+    template5, context5 = captured_templates[4]
+    assert template5.name == "booking.html"
